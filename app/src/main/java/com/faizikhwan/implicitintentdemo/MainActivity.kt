89d3d5846455a99971fb48543dfebda60da2dc66
Faiz Ikhwan <faizikhwantop@gmail.com>
2020-01-28 09:27:49 +0800

    Finished.

--- app/src/main/java/com/faizikhwan/implicitintentdemo/MainActivity.kt
+++ app/src/main/java/com/faizikhwan/implicitintentdemo/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.faizikhwan.implicitintentdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_share.setOnClickListener {
+            val message = et_message.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to:"))
+        }
     }
 }
